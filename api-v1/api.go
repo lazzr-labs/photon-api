d560ea0591808f7481eb198a0a15e13948e60dba
Miguel Vazquez <migueldv90@gmail.com>
2026-07-08 12:05:29 -0500

    update

--- api-v1/api.go
+++ api-v1/api.go
@@ -18,7 +18,6 @@ import (
 	"api-go/routes/home_api"
 	"api-go/routes/places_api"
 	"api-go/routes/users_api"
-	"api-go/scripts/emails"
 	"api-go/utils/auth"
 	"api-go/utils/cache"
 	"api-go/utils/db"
@@ -42,9 +41,7 @@ func main() {
 		db.SetEntDB(cfg.Database)
 		auth.SetSecret(cfg.Secret)
 		cache.SetClient(cfg.Valkey)
-		if err := emails.SetClient(cfg.SendGridAPIKey, cfg.EmailFromName, cfg.EmailFromEmail); err != nil {
-			panic("failed to configure email client")
-		}
+		// emails.SetApiKey(cfg.SENDGRID)
 		// places.SetClient(cfg.GoogleMapsAPIKey)
 		// auth.SetSupabaseSecret(cfg.SupabaseSecret)
 		// auth.SetSupabaseIssuer(cfg.SupabaseIssuer)
